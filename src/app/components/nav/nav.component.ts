b2dfa00e5e2bf00827a57bc23db9bc4303071fe7
Erick Marín <erick.marin.v@gmail.com>
2022-10-12 19:55:50 -0600

    UI de la barra de navegacion

---
+++ src/app/components/nav/nav.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { faChevronLeft } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.scss']
+})
+export class NavComponent implements OnInit {
+  faChevronLeft = faChevronLeft;
+
+  titulo = 'Wallet';
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
